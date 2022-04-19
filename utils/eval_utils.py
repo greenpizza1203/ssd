41644409e1ce9c03cac4fb57cbe6117110539584
Karthikeya Kona <konakarthik12@gmail.com>
2022-04-19 16:48:37 -0400

    yep

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from tqdm import tqdm
 from utils import bbox_utils
 
 def init_stats(labels):
@@ -86,7 +87,7 @@ def calculate_mAP(stats):
 
 def evaluate_predictions(dataset, pred_bboxes, pred_labels, pred_scores, labels, batch_size):
     stats = init_stats(labels)
-    for batch_id, image_data in enumerate(dataset):
+    for batch_id, image_data in tqdm(enumerate(dataset)):
         imgs, gt_boxes, gt_labels = image_data
         start = batch_id * batch_size
         end = start + batch_size
