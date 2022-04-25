5ac91b9572c8cad90f39e6f2e848ad79281a81b7
Karthikeya Kona <konakarthik12@gmail.com>
2022-04-25 16:19:06 -0400

    try

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='kaggle_ssd',
+    name='models',
     #    packages=find_packages(),
     version='0.1.0',
     description='My first Python library',
