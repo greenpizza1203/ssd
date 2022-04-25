4aac66908c310c5086616c7963166bc00493c106
Karthikeya Kona <konakarthik12@gmail.com>
2022-04-25 16:03:18 -0400

    try

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import find_packages, setup
+
 setup(
     name='colab',
-#    packages=find_packages(),
+    #    packages=find_packages(),
     version='0.1.0',
     description='My first Python library',
     author='Me',
     license='MIT',
+    packages=['colab']
+
 )
