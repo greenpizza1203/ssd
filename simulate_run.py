47f493f4abc7fc45c6ac76461f558e346dd07c63
Karthikeya Kona <konakarthik12@gmail.com>
2022-04-29 11:48:38 -0400

    huh

--- simulate_run.py
+++ simulate_run.py
@@ -3,11 +3,12 @@ from trainer import Trainer
 batch_size = 16
 epochs = 1
 
-from custom_datasets import Voc
+from custom_datasets import voc
 
-voc = Voc()
-train_data, train_size_estimate, val_data, val_size_estimate = voc.train_data, voc.train_size, voc.val_data, voc.val_size
+voc.load_datasets()
+print(next(iter(voc.train.data)))
+# train_data, train_size_estimate, val_data, val_size_estimate = voc.train_data, voc.train_size, voc.val_data, voc.val_size
 
-trainer = Trainer(epochs, batch_size)
-trainer.load_data(train_data, val_data, labels)
-trainer.fit()
+# trainer = Trainer(epochs, batch_size)
+# trainer.load_data(train_data, val_data, labels)
+# trainer.fit()
