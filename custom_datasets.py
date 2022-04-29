3f4c6a04191936198a86993fc6f40935fef86181
Karthikeya Kona <konakarthik12@gmail.com>
2022-04-28 22:52:58 -0400

    try_it

--- custom_datasets.py
+++ custom_datasets.py
@@ -4,6 +4,18 @@ import tensorflow_datasets as tfds
 image_size = (300, 300)
 
 
+def load_dataset(name, split):
+    data, info = tfds.load(name, split=split, with_info=True)
+
+    labels = info.features["labels"].names
+
+    total = 0
+    for part in split.split("+"):
+        total += info.splits[part].num_examples
+
+    return data, total, labels
+
+
 def resize_image(element):
     image = tf.image.convert_image_dtype(element['image'], tf.float32)
     resized_image = tf.image.resize(image, image_size)
@@ -11,14 +23,16 @@ def resize_image(element):
     return fixed_image
 
 
-train_data, info = tfds.load("voc/2007", split="train+validation", with_info=True)
+class Voc:
+    def __init__(self):
+        train_data, train_size, labels = load_dataset("voc/2007", "train+validation")
+        train_2012, train_2012_size, _ = load_dataset("voc/2012", "train+validation")
 
-voc_2012_data, voc_2012_info = tfds.load("voc/2012", split="train+validation", with_info=True)
+        train_data = train_data.concatenate(train_2012).map(resize_image)
 
-voc_2012_total_items = voc_2012_info.splits["train"].num_examples + voc_2012_info.splits["validation"].num_examples
+        val_data, _, _ = load_dataset("voc/2007", "test")
 
-train_data = train_data.concatenate(voc_2012_data).map(resize_image)
-
-val_data = tfds.load("voc/2007", split="test").map(resize_image)
-
-labels = info.features["labels"].names
+        val_data = val_data.map(resize_image)
+        self.train_data = train_data
+        self.train_size = train_size + train_2012_size
+        self.val_data = val_data
