6b242d131b833d06d9d08c8d2e03b2e6901c15bb
Karthikeya Kona <konakarthik12@gmail.com>
2022-04-29 11:44:04 -0400

    woops

--- custom_datasets/voc.py
+++ custom_datasets/voc.py
@@ -19,11 +19,11 @@ def load_datasets():
     global train, val, labels
     train_data, train_size, labels = load_dataset("voc/2007", "train+validation")
     train_2012, train_2012_size, _ = load_dataset("voc/2012", "train+validation")
-    train_data = train_data.concatenate(train_2012).map(resize_image)
+    train_data = train_data.concatenate(train_2012).map(filter_features)
     train_size += train_2012_size
 
     val_data, val_size, _ = load_dataset("voc/2007", "test")
-    val_data = val_data.map(resize_image)
+    val_data = val_data.map(filter_features)
 
     train = Split(train_data, train_size)
     val = Split(val_data, val_size)
