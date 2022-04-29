2209498edcb526e44f0d41e1e5cb85c40a61c9e3
Karthikeya Kona <konakarthik12@gmail.com>
2022-04-29 11:33:01 -0400

    clear

--- trainer.py
+++ trainer.py
@@ -14,7 +14,7 @@ class Trainer:
         self.fitter = None
 
     def load_data(self, dataset):
-        train_data, val_data, labels = dataset.train_data, dataset.val_data, dataset.labels
+        train_data, val_data, labels = dataset.train.data, dataset.val.data, dataset.labels
         labels = ["bg"] + labels
         hyper_params = train_utils.get_hyper_params()
 
@@ -24,8 +24,8 @@ class Trainer:
         train_data = train_data.map(lambda x: data_utils.preprocessing(x, img_size, img_size, augmentation.apply))
         val_data = val_data.map(lambda x: data_utils.preprocessing(x, img_size, img_size))
 
-        train_total_items = train_data.reduce(0, lambda x, _: x + 1)
-        val_total_items = val_data.reduce(0, lambda x, _: x + 1)
+        train_total_items = dataset.train.size
+        val_total_items = dataset.val.size
 
         data_shapes = data_utils.get_data_shapes()
         padding_values = data_utils.get_padding_values()
@@ -38,7 +38,6 @@ class Trainer:
                           loss=[ssd_custom_losses.loc_loss_fn, ssd_custom_losses.conf_loss_fn])
         init_model(ssd_model)
         #
-        ssd_model_path = "ssd_weight.h5"
 
         # We calculate prior boxes for one time and use it for all operations because of the all images are the same sizes
         prior_boxes = bbox_utils.generate_prior_boxes(hyper_params["feature_map_shapes"], hyper_params["aspect_ratios"])
