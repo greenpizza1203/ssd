796b17ccecc612f41c130c90f060d2611109a1e6
julianf20 <98412392+julianf20@users.noreply.github.com>
2022-05-14 17:08:03 -0400

    Update ssd_mobilenet_v2.py

--- models/ssd_mobilenet_v2.py
+++ models/ssd_mobilenet_v2.py
@@ -19,17 +19,17 @@ def get_model(hyper_params):
     second_conv = base_model.output
     #
     ############################ Extra Feature Layers Start ############################
-    extra1_1 = Conv2D(256, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra1_1")(second_conv)
-    extra1_2 = Conv2D(512, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra1_2")(extra1_1)
+    extra1_1 = Conv2D(128, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra1_1")(second_conv)
+    extra1_2 = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra1_2")(extra1_1)
     #
-    extra2_1 = Conv2D(128, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra2_1")(extra1_2)
-    extra2_2 = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra2_2")(extra2_1)
+    extra2_1 = Conv2D(64, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra2_1")(extra1_2)
+    extra2_2 = Conv2D(128, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra2_2")(extra2_1)
     #
-    extra3_1 = Conv2D(128, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra3_1")(extra2_2)
-    extra3_2 = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra3_2")(extra3_1)
+    extra3_1 = Conv2D(64, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra3_1")(extra2_2)
+    extra3_2 = Conv2D(128, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra3_2")(extra3_1)
     #
-    extra4_1 = Conv2D(128, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra4_1")(extra3_2)
-    extra4_2 = Conv2D(256, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra4_2")(extra4_1)
+    extra4_1 = Conv2D(64, (1, 1), strides=(1, 1), padding="valid", activation="relu", name="extra4_1")(extra3_2)
+    extra4_2 = Conv2D(128, (3, 3), strides=(2, 2), padding="same", activation="relu", name="extra4_2")(extra4_1)
     ############################ Extra Feature Layers End ############################
     pred_deltas, pred_labels = get_head_from_outputs(hyper_params, [first_conv, second_conv, extra1_2, extra2_2, extra3_2, extra4_2])
     return Model(inputs=input, outputs=[pred_deltas, pred_labels])
